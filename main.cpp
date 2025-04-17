9d7555a9a3190db3cc3c8e1166c2db70f91682aa
chakkad <8b.igustibaguschakkad@gmail.com>
2025-04-17 14:09:27 +0200

    Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,10 @@
-#include "ensemble.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
-using namespace std;
+#include "coord.hpp" 
+#include <iostream>
 
-Ensemble::Ensemble() : card(0) {}
-
-bool Ensemble::estVide() const { return card == 0; }
-int Ensemble::cardinal() const { return card; }
-
-void Ensemble::ajoute(int element) {
-    if (card >= MAXCARD) {
-        throw runtime_error("Ensemble plein");
-    }
-    t[card++] = element;
-}
-
-int Ensemble::tire() {
-    if (estVide()) {
-        throw runtime_error("Ensemble vide");
-    }
-    srand(time(nullptr));
-    int index = rand() % card;
-    int value = t[index];
-    t[index] = t[card - 1];
-    card--;
-    return value;
-}
-
-ostream& operator<<(ostream& os, const Ensemble& e) {
-    os << "{ ";
-    for (int i = 0; i < e.card; ++i) {
-        os << e.t[i] << " ";
-    }
-    os << "}";
-    return os;
-}
-
-// Tests à mettre dans test.cpp
-TEST_CASE("Constructeur par défaut") {
-    Ensemble e;
-    CHECK(e.estVide());
-    CHECK(e.cardinal() == 0);
-}
-
-TEST_CASE("Ajoute et cardinal") {
-    Ensemble e;
-    e.ajoute(5);
-    CHECK(e.cardinal() == 1);
-    CHECK_FALSE(e.estVide());
-}
-
-TEST_CASE("Exceptions") {
-    Ensemble e;
-    CHECK_THROWS_AS(e.tire(), runtime_error);
-    for (int i = 0; i < MAXCARD; i++) e.ajoute(i);
-    CHECK_THROWS_AS(e.ajoute(MAXCARD), runtime_error);
+int main() {
+	Coord c(23,24);
+	cout << "Coordonnees : " << c << endl;
+	return 0;
 }
